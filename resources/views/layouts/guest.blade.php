bcba4add9200054bc754c03efad3ae921cb5e121
Ohislawrence <lawrenceohis@gmail.com>
2025-06-04 08:04:18 +0100

    google ana add

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6RYMDQ1QH5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6RYMDQ1QH5');
+</script>
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -9,7 +18,7 @@
         @include('partials.css')
     @yield('header')
     <meta name="p:domain_verify" content="15443de08218d316c37d51c296778b77"/>
-    <meta name='impact-site-verification' value='7e9b2beb-9683-476d-b2a1-358114b443f5'>
+    
 </head>
 <body>
     <!-- Header -->
